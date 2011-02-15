e4dd00268bb5bdbb827be3cf2b9071a651241ac5
David Mills <david.mills@velir.com>
2011-02-15 00:25:05 -0500

    basic implementation of an IRequestBuilder

---
+++ source/Brightcove4net/Api/Connectors/BasicRequestBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Brightcove4net.Util;
+
+namespace Brightcove4net.Api.Connectors
+{
+	public class BasicRequestBuilder : IRequestBuilder
+	{
+		public BrightcoveApiConfig Configuration
+		{
+			get;
+			set;
+		}
+
+		public BasicRequestBuilder(BrightcoveApiConfig configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public virtual HttpWebRequest BuildRequest(string url)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Timeout = Configuration.RequestTimeout;
+			return request;
+		}
+	}
+}
