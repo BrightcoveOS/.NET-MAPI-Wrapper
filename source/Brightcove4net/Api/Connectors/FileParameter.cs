4a4168ef4e90b06b9607c5fc769c1448d685ac73
David Mills <david.mills@velir.com>
2011-02-15 02:27:48 -0500

    FileParameter class now resides in it's own separate file

---
+++ source/Brightcove4net/Api/Connectors/FileParameter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Brightcove4net.Api.Connectors
+{
+	public class FileParameter
+	{
+		public byte[] File { get; set; }
+		public string FileName { get; set; }
+		public string ContentType { get; set; }
+
+		public FileParameter(byte[] file)
+			: this(file, null)
+		{
+		}
+
+		public FileParameter(byte[] file, string filename)
+			: this(file, filename, null)
+		{
+		}
+
+		public FileParameter(byte[] file, string filename, string contenttype)
+		{
+			File = file;
+			FileName = filename;
+			ContentType = contenttype;
+		}
+	}
+}
