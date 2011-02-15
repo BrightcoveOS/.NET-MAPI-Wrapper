209ab48c12ac716eab6571d64f9909d1c2e676a1
David Mills <david.mills@velir.com>
2011-02-15 00:26:36 -0500

    Added an interface for a class to build web requests

---
+++ source/Brightcove4net/Api/Connectors/IRequestBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Brightcove4net.Api.Connectors
+{
+	public interface IRequestBuilder
+	{
+		HttpWebRequest BuildRequest(string url);
+	}
+}
