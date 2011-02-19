a9bc85cd61de99617203877f68dc9a20b52da4e2
David Mills <david.mills@velir.com>
2011-02-19 16:16:52 -0500

    incremented version to RC3

--- source/BrightcoveOS .NET-MAPI-Wrapper/Properties/AssemblyInfo.cs
+++ source/BrightcoveOS .NET-MAPI-Wrapper/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("1.0 RC2")]
+[assembly: AssemblyInformationalVersion("1.0 RC3")]
