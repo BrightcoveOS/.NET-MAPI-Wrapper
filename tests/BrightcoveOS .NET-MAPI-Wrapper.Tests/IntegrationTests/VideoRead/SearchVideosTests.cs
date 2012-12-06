b849cd293c43b13b94d694d1a767e2ebd1712923
Peter Majeed <peter.majeed@velir.com>
2012-12-06 12:26:41 -0500

    Added a unit test project to the solution.

---
+++ tests/BrightcoveOS .NET-MAPI-Wrapper.Tests/IntegrationTests/VideoRead/SearchVideosTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BrightcoveOS.NET_MAPI_Wrapper.Tests.IntegrationTests.VideoRead
+{
+    [TestFixture]
+    public class SearchVideosTests
+    {
+        [Test]
+        public void SearchVideos_Basic_Test()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
