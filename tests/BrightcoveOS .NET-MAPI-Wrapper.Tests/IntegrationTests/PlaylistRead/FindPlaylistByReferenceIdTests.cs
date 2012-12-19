94e4e5d09c4383e067c1a18e9d8e1c785d777426
dmillz <github@millz.com>
2012-12-19 13:28:00 -0500

    Integration tests for playlist read calls

---
+++ tests/BrightcoveOS .NET-MAPI-Wrapper.Tests/IntegrationTests/PlaylistRead/FindPlaylistByReferenceIdTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrightcoveMapiWrapper.Model.Items;
+using NUnit.Framework;
+
+namespace BrightcoveOS.NET_MAPI_Wrapper.Tests.IntegrationTests.PlaylistRead
+{
+	[TestFixture]
+	public class FindPlaylistByReferenceIdTests : PlaylistReadTestBase
+	{
+		[Test]
+		public void FindPlaylistByRefereceId_Test_Basic()
+		{
+			BrightcovePlaylist playlist = _api.FindPlaylistByReferenceId("1963943648001");
+
+			Assert.NotNull(playlist);
+			Assert.Greater(playlist.VideoIds.Count, 0);
+			Assert.Greater(playlist.Videos.Count, 0);
+		}
+	}
+}
---
+++ tests/BrightcoveOS .NET-MAPI-Wrapper.Tests/IntegrationTests/PlaylistRead/FindPlaylistsByIdsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BrightcoveOS.NET_MAPI_Wrapper.Tests.IntegrationTests.PlaylistRead
+{
+	[TestFixture]
+	public class FindPlaylistsByIdsTests : PlaylistReadTestBase
+	{
+		[Test]
+		public void FindPlaylistsByIds_Test_Basic()
+		{
+			_playlists = _api.FindPlaylistsByIds(new[] { 1963943648001, 1963943652001 });
+
+			Assert.AreEqual(2, _playlists.Count);
+			Assert.AreEqual(0, _playlists.Where(o => o == null).Count());
+		}
+	}
+}
