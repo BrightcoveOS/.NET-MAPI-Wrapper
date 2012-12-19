4aec4ba7f7a935a525826b53f2edf36a9e262825
dmillz <github@millz.com>
2012-12-19 13:14:33 -0500

    Integration tests for video write calls

---
+++ tests/BrightcoveOS .NET-MAPI-Wrapper.Tests/IntegrationTests/VideoWrite/DeleteVideoTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BrightcoveOS.NET_MAPI_Wrapper.Tests.IntegrationTests.VideoWrite
+{
+	[TestFixture]
+	public class DeleteVideoTests : VideoWriteTestBase
+	{
+		[Test]
+		public void DeleteVideo_Test_Basic()
+		{
+			long videoId = 1939484105001; // test video from http://docs.brightcove.com/en/media/samples/delete_video.html
+
+			// delete it
+			_api.DeleteVideo(videoId, true, true);
+
+			// verify it's gone
+			Assert.IsNull(_api.FindVideoById(videoId));
+		}
+	}
+}
